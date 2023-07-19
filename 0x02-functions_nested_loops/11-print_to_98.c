68e73319b44631108c6f232bed7bb7dbc9cb8441
Teeegirl <Omotoyosi0201@gmail.com>
2023-07-18 18:47:38 -0700

    God

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * print_to_98 - prints all natural numbers from n to 98,
  * followed by a new line
@@ -29,4 +28,4 @@ void print_to_98(int n)
 				printf("%d\n", j);
 		}
 	}
-}		
+}
