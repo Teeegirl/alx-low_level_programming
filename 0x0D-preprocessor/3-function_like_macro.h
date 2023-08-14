84abe9890f9eda4216b3b7a26ca1f223ed46fc0c
Teeegirl <Omotoyosi0201@gmail.com>
2023-08-14 14:49:20 -0700

    it

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,12 +1,6 @@
-#ifndef MACROLIKE_H
-#define MACROLIKE_H
-/**
-  * file: 3-function_like_macro
-  *
-  * @x: input value (an int)
-  *
-  * desc: header file that computes absolute value
-  */
-#define ABS(x) ((x) < 0 ? -(1) * (x) :(x))
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x) < (0) ? -(x) : (x))
 
 #endif
