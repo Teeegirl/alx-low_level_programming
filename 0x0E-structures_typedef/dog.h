5bd9e7b391a641e60c55e4430f00cad240c25cf5
Teeegirl <Omotoyosi0201@gmail.com>
2023-08-14 17:12:22 -0700

    0x0E. C - Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+  * struct dog - a struct that creates the dog info
+  *
+  * @name: name of the dog
+  * @age: age of the dog
+  * @owner: owner of the dog
+  *
+  * Description: first struct
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
